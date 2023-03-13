951b06eeb83475a786016ff15316fd9cd1934cbb
Francisnei <francisnei@zapt.tech>
2023-03-13 13:15:50 -0300

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-// ignore: depend_on_referenced_packages
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:zapt_sdk_flutter/zapt_sdk_flutter.dart';
 
 void main() {
@@ -20,7 +18,6 @@ class Example extends StatefulWidget {
 class _ExampleState extends State<Example> {
   static const stream = EventChannel("ReactNativeZaptSdkBeaconsFound");
   String _mapLink = "";
-  WebViewController? controller;
   int _selectedIndex = 0;
   final _zaptSDKPlugin = ZaptSdkFlutter();
   Map<String, String> options = {
@@ -33,36 +30,12 @@ class _ExampleState extends State<Example> {
   };
   final String placeId = "-ltvysf4acgzdxdhf81y";
 
-  final List<Widget> _telas = [];
 
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
     _zaptSDKPlugin.requestPermissionsBackground({});
-    initScreens();
     getMapLink(placeId, options);
-  }
-
-  void initScreens() {
-    _telas.add(ZaptMap(placeId: placeId, options: options));
-    _telas.add(WebView(
-      onWebViewCreated: (WebViewController webViewController) {
-        controller = webViewController;
-        if (_mapLink.isNotEmpty) {
-          _zaptSDKPlugin.requestPermissions();
-          webViewController.loadUrl(_mapLink);
-        }
-      },
-      onPageFinished: ((String url) => initListenBeacons()),
-      javascriptMode: JavascriptMode.unrestricted,
-    ));
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 
   Future<void> getMapLink(placeId, options) async {
@@ -74,9 +47,6 @@ class _ExampleState extends State<Example> {
           "";
     } on PlatformException {
       mapLink = 'Error';
-    }
-    if (mapLink.isNotEmpty) {
-      controller?.loadUrl(mapLink);
     }
     setState(() {
       _mapLink = mapLink;
@@ -95,22 +65,15 @@ class _ExampleState extends State<Example> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Zapt SDK Flutter Example"),
-            ),
-            body: Center(
-              child: _telas[_selectedIndex],
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.map), label: "Zapt Map"),
-                BottomNavigationBarItem(icon: Icon(Icons.web), label: "WebView")
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            )));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Zapt SDK Flutter Example"),
+        ),
+        body: Center(
+          child: ZaptMap(placeId: placeId, options: options),
+        ),
+      ),
+    );
   }
 }
